cdb05872f522eb2fdfd33d938abd78dd667abd07
DarkBoyz <113273465+DarkBoyz@users.noreply.github.com>
2022-09-26 15:31:59 +0700

    Update _menunya.js

--- plugins/_menunya.js
+++ plugins/_menunya.js
@@ -175,8 +175,24 @@ let handler = async (m, { conn, usedPrefix: _p }) => {
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
     let fetch = require('node-fetch') 
+    const ftroli = {
+	key : {
+                          participant : '0@s.whatsapp.net'
+                        },
+       message: {
+                    orderMessage: {
+                            itemCount : 9999999999999,
+                            status: 1,
+                            surface : 1,
+                            message: '© Boba Bot V1.05',
+                            orderTitle: 'MENU',
+                            thumbnail: await (await fetch('https://telegra.ph/file/b5064fec4c12e77672585.jpg')).buffer(),
+                            sellerJid: '0@s.whatsapp.net'
 
-await conn.send3ButtonImg(m.chat, await (await fetch('https://telegra.ph/file/b5064fec4c12e77672585.jpg')).buffer(),  '*────────[ DASBOARD ]────────*', text, 'OWNER', '.owner', 'INFOBOT', '.info', 'DONASI', '.donasi', m,)
+                          }
+                        }
+                      }
+await conn.send3ButtonImg(m.chat, await (await fetch('https://telegra.ph/file/c6801d9fd7d2d27f1b43a.jpg')).buffer(),  '*────────[ DASBOARD ]────────*', text, 'OWNER', '.owner', 'INFOBOT', '.info', 'DONASI', '.donasi', m,)
   } catch (e) {
     conn.reply(m.chat, 'Maaf, menu server sedang error', m)
     throw e
